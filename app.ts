8f52653719deea8fe423d5ff5e4b9bb5fb43c7de
Beny <b.ntambwe97@gmail.com>
2022-10-10 18:01:18 +0200

    Function as Types

--- app.ts
+++ app.ts
@@ -6,4 +6,14 @@ function printResult(num: number): void {
     console.log('Result: ' + num);
 }
 
-console.log(printResult(add(5, 12)));
+printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8))
+
+// let someValue: undefined;
