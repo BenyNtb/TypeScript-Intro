ad0af9d898406ed0fdd3f4b1325786360502cbd2
Beny <b.ntambwe97@gmail.com>
2022-10-10 17:02:40 +0200

    Literal Types

--- app.ts
+++ app.ts
@@ -1,47 +1,27 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-//     const person: {
-//         name: string;
-//         age: number;
-//         hobbies: string[];
-//         role: [number, string];
-//     } = {
-//     name: 'Beny',
-//     age: 25,
-//     hobbies: ['Programming', 'Arts'],
-//     role: [2, 'author']
-// };
-
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role { ADMIN, READ_ONLY, AUTHOR };
-
-const person = {
-name: 'Beny',
-age: 25,
-hobbies: ['Programming', 'Arts'],
-role: Role.ADMIN
-};
-
-// person.role.push('admin');
-// person.role[1] = 10;
-
-// person.role = [0, 'admin', 'user'];
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
-
-console.log(person.name);
-
-for(const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); !! ERROR !!
+function combine(
+    input1: number | string, 
+    input2: number | string, 
+    resultConversion: 'as-number' | 'as-text'
+    ) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    return result; 
+    // if (resultConversion === 'as-number') {
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
 }
 
-if (person.role === Role.AUTHOR) {
-    console.log('is author');
-}
+const combinedAges = combine(30, 25, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combine('30', '25', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine ('Beny', 'Sarah', 'as-text');
+console.log(combinedNames);
