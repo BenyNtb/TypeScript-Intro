63bb39ab83a6d996d3af32399d1cf2561f91679f
Beny <b.ntambwe97@gmail.com>
2022-10-10 18:32:17 +0200

    The 'never' Types

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 var userInput;
+var userName;
 userInput = 5;
 userInput = 'Beny';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+var result = generateError('An error occured!', 500);
+console.log(result);
